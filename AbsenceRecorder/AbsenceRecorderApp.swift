24970fe96356bd224a5503016a53a699bbcc8461
Sean Okuyama-Smith <okuyama-smith.s@etoncollege.org.uk>
2022-01-24 21:52:07 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Okuyama-Smith, Sean (NA) on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
